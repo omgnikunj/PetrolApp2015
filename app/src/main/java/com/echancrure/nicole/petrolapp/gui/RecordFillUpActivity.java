6f73b65527b11a562b214bd5d97524924902d07f
Chris Meudec <echancrure@gmail.com>
2015-08-27 11:32:31 +0100

    Added basic design (need to fixed) for fill-up sumission and skeleton code to handle a submission.

--- app/src/main/java/com/echancrure/nicole/petrolapp/gui/RecordFillUpActivity.java
+++ app/src/main/java/com/echancrure/nicole/petrolapp/gui/RecordFillUpActivity.java
@@ -2,38 +2,78 @@ package com.echancrure.nicole.petrolapp.gui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.echancrure.nicole.petrolapp.R;
 
+import services.UTCTime;
+
 public class RecordFillUpActivity extends AppCompatActivity {
+    private final static String TAG = "RecordFillUpActivity";
+    /** time formatter */
+    private UTCTime uTCTimeFormatter = new UTCTime();
+    /** the UTC date and time of the fill up */
+    private long uTCDateTime;
+    /** the date picker */
+    private DatePicker dateTimeTextView;
+    /** the view holding the odometer reading */
+    private TextView odometerTextView;
+    /** the view holding the price reading */
+    private TextView priceTextView;
+    /** the view holding the volume reading */
+    private TextView volumeTextView;
+    /** the odometer value to save */
+    private int odometer;
+    /** the price to save */
+    private int price;
+    /** the volume to save */
+    private float volume;
+    /** the partial indication of the fill up */
+    private boolean partial;
+    /** the fill up object created */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_fill_up);
+        this.uTCDateTime = System.currentTimeMillis();
+        this.dateTimeTextView = (DatePicker) findViewById(R.id.datePicker);
+        this.odometerTextView = (TextView) findViewById(R.id.odometerField);
+        this.volumeTextView = (TextView) findViewById(R.id.volumeField);
+        this.priceTextView = (TextView) findViewById(R.id.priceField);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_record_fill_up, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    /**
+     * handler for all the views on the screen
+     * @param view the sender
+     */
+    public void onClick(View view){
+        Log.d(TAG, "onClick received");
+        switch (view.getId()) {
+            case R.id.submitButton:
+                handleSubmitButtonOnClick();
+                break;
+            case R.id.cancelButton:
+                this.finish();
+                break;
+            case R.id.cameraButton:
+                Toast.makeText(RecordFillUpActivity.this, "Camera function not yet implemented", Toast.LENGTH_LONG).show();
+                break;
+            default:
+                Log.d(TAG, "ignored click event on:" + view.getId());
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    /**
+     * handle a fill-up submit: check that all the fields are in the correct format and send to the controller
+     */
+    private void handleSubmitButtonOnClick() {
+        //TODO
     }
 }
